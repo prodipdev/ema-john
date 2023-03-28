762105fd1bf070c7e1a9727525adf390c5536221
prodipdev <prodipink@gmail.com>
2023-03-28 13:42:01 +0600

    Added cart calculation and store cart in localStorage.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
   const cart = props.cart;
-  const priceList = cart.map((product) => product.price);
-  const totalPrice = priceList.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  }, 0);
+
+  let quantity = 0;
+  let totalPrice = 0;
+  let totalShipping = 0;
+  for (const product of cart) {
+    quantity += product.quantity;
+    console.log("quantity- ");
+    console.log(product.quantity);
+
+    totalPrice += product.price * product.quantity;
+    totalShipping += product.shipping;
+  }
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShipping + tax;
+
   return (
     <div className="cart-container">
       <h2>Order Summary</h2>
-      <h3>Selected Items: {cart.length}</h3>
-      <h3>Total Price: ${totalPrice}</h3>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
     </div>
   );
 };
